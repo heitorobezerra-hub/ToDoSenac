aee1a06b6e960f190e449281ea85688c26ceb24a
Heitor de Oliveira <heitor.obezerra@gmail.com>
2025-11-18 11:48:47 -0300

    Create URLS
    I created an URLS and left the white page to insert some desings tomorrow

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('',HomeView.as_view(),name='home')
 ]
