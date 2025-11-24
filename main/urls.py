30eba8d41de9f0cb0a06d2d036130828c158ae73
Heitor de Oliveira Bezerra <heitor.obezerra@gmail.com>
2025-11-24 11:25:06 -0300

    Create Filters
    I create a filter for completed and pending tasks

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from main.views import TaskList
+from main.views import TaskList, task_concluido, task_pendente
+from .import views
 
 urlpatterns = [
-    path('', TaskList.as_view(), name = 'task_list')
+    path('', TaskList.as_view(), name = 'task_list'),
+    path('concluido/', views.task_concluido, name = 'task_concluido'),
+    path('pendente/', views.task_pendente, name= 'task_pendente')
 ]
 
 
--- main/views.py
+++ main/views.py
@@ -9,3 +9,27 @@ class TaskList(ListView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tarefas'
+
+def task_concluido(request):
+    a = 4
+    b = 5
+    c = a + b
+
+    tarefas = Task.objects.filter(concluida=1)
+    context = {
+        "tarefas":tarefas,
+        "título_pagina": "Minhas tarefas concluidas",
+    }
+    return render(request, 'tasks/teste.html', context)
+
+def task_pendente(request):
+    a = 4
+    b = 5
+    c = a + b
+
+    tarefas = Task.objects.filter(concluida=0)
+    context = {
+        "tarefas":tarefas,
+        "título_pagina": "Minhas tarefas concluidas",
+    }
+    return render(request, 'tasks/teste2.html', context)
